a580c64968146cb1e02b843c2075f172a42cde40
SbangariGIT <56751089+sbangariGIT@users.noreply.github.com>
2021-10-28 12:33:55 -0400

    initial commit

---
+++ android/app/src/main/kotlin/com/example/jump_mounish/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.jump_mounish
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Expanded(
+            child: Container(
+              color: Colors.black,
+            ),
+          ),
+          Expanded(
+            child: Container(
+              color: Colors.white,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
