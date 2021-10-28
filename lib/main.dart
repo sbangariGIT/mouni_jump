574966e4729e6725514d2a40c21c3f997d3cc220
SbangariGIT <56751089+sbangariGIT@users.noreply.github.com>
2021-10-28 18:03:31 -0400

    adding gravity

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:jump_mounish/mouni.dart';
 
@@ -24,6 +26,34 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  static double yAxis = 0;
+  double score = 0;
+  double height = 0;
+  double inHeight = yAxis;
+  bool gameInSession = false;
+
+  void tap() {
+    setState(() {
+      score = 0;
+      inHeight = yAxis;
+    });
+  }
+
+  void initializeGame() {
+    gameInSession = true;
+    Timer.periodic(Duration(milliseconds: 60), (timer) {
+      score += 0.05;
+      height = (-4.9 * score * score) + (2.8 * score);
+      setState(() {
+        yAxis = inHeight - height;
+      });
+      if (yAxis > 1) {
+        timer.cancel();
+        gameInSession = false;
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,14 +61,35 @@ class _HomePageState extends State<HomePage> {
         children: [
           Expanded(
             flex: 2,
-            child: Container(
-              color: Colors.black,
-              child: Center(child: Mouni()),
+            child: GestureDetector(
+              onTap: () {
+                if (gameInSession) {
+                  tap();
+                } else {
+                  initializeGame();
+                }
+              },
+              child: AnimatedContainer(
+                alignment: Alignment(0, 0),
+                duration: Duration(milliseconds: 0),
+                color: Colors.black,
+                child: Center(child: Mouni()),
+              ),
             ),
           ),
           Expanded(
             child: Container(
               color: Colors.white,
+              child: Row(
+                children: [
+                  Column(
+                    children: [Text("Score"), Text("0")],
+                  ),
+                  Column(
+                    children: [Text("Best"), Text("0")],
+                  ),
+                ],
+              ),
             ),
           ),
         ],
