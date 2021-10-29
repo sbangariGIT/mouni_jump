21ce47e795a070d7959e2e6e46d1b44f60e3d0d5
SbangariGIT <56751089+sbangariGIT@users.noreply.github.com>
2021-10-28 20:03:02 -0400

    adding barriers

--- lib/main.dart
+++ lib/main.dart
@@ -31,6 +31,10 @@ class _HomePageState extends State<HomePage> {
   double height = 0;
   double inHeight = yAxis;
   bool gameInSession = false;
+  double x1 = 0.2;
+  double x2 = 0.2;
+  double x3 = 1;
+  double x4 = 1;
 
   void tap() {
     setState(() {
@@ -43,56 +47,156 @@ class _HomePageState extends State<HomePage> {
     gameInSession = true;
     Timer.periodic(Duration(milliseconds: 60), (timer) {
       score += 0.05;
-      height = (-4.9 * score * score) + (2.8 * score);
+      height = -4.9 * score * score + 2.8 * score;
       setState(() {
         yAxis = inHeight - height;
       });
       if (yAxis > 1) {
         timer.cancel();
-        gameInSession = false;
+        setState(() {
+          gameInSession = false;
+          yAxis = 0;
+          inHeight = yAxis;
+          score = 0;
+          height = 0;
+        });
       }
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        children: [
-          Expanded(
-            flex: 2,
-            child: GestureDetector(
-              onTap: () {
-                if (gameInSession) {
-                  tap();
-                } else {
-                  initializeGame();
-                }
-              },
-              child: AnimatedContainer(
-                alignment: Alignment(0, 0),
-                duration: Duration(milliseconds: 0),
-                color: Colors.black,
-                child: Center(child: Mouni()),
+    return GestureDetector(
+      onTap: () {
+        //print("i AM Here");
+        if (gameInSession) {
+          //print("i AM tap");
+          tap();
+        } else {
+          //print("i AM in");
+          initializeGame();
+        }
+      },
+      child: Scaffold(
+        body: Column(
+          children: [
+            Expanded(
+              flex: 2,
+              child: Stack(children: [
+                AnimatedContainer(
+                  alignment: Alignment(0, yAxis),
+                  duration: Duration(milliseconds: 0),
+                  color: Colors.black,
+                  child: Mouni(),
+                ),
+                Container(
+                  alignment: Alignment(0, -0.3),
+                  child: gameInSession
+                      ? Container()
+                      : Text(
+                          "Tap To Make Mouni jump",
+                          style: TextStyle(color: Colors.white),
+                        ),
+                ),
+                AnimatedContainer(
+                  alignment: Alignment(x1, -1),
+                  duration: Duration(milliseconds: 0),
+                  child: Barrier(
+                    size: 100.0,
+                  ),
+                ),
+                AnimatedContainer(
+                  alignment: Alignment(x2, 1),
+                  duration: Duration(milliseconds: 0),
+                  child: Barrier(
+                    size: 100.0,
+                  ),
+                ),
+                AnimatedContainer(
+                  alignment: Alignment(x3, -1),
+                  duration: Duration(milliseconds: 0),
+                  child: Barrier(
+                    size: 150.0,
+                  ),
+                ),
+                AnimatedContainer(
+                  alignment: Alignment(x4, 1),
+                  duration: Duration(milliseconds: 0),
+                  child: Barrier(
+                    size: 100.0,
+                  ),
+                ),
+                // AnimatedContainer(
+                //   alignment: Alignment(-0.7, -1),
+                //   duration: Duration(milliseconds: 0),
+                //   child: Barrier(
+                //     size: 200.0,
+                //   ),
+                // ),
+              ]),
+            ),
+            Expanded(
+              child: Container(
+                color: Colors.white,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          "Score",
+                          style: TextStyle(
+                            fontSize: 30,
+                          ),
+                        ),
+                        Text(
+                          "0",
+                          style: TextStyle(
+                            fontSize: 30,
+                          ),
+                        )
+                      ],
+                    ),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          "Best",
+                          style: TextStyle(
+                            fontSize: 30,
+                          ),
+                        ),
+                        Text(
+                          "0",
+                          style: TextStyle(
+                            fontSize: 30,
+                          ),
+                        )
+                      ],
+                    ),
+                  ],
+                ),
               ),
             ),
-          ),
-          Expanded(
-            child: Container(
-              color: Colors.white,
-              child: Row(
-                children: [
-                  Column(
-                    children: [Text("Score"), Text("0")],
-                  ),
-                  Column(
-                    children: [Text("Best"), Text("0")],
-                  ),
-                ],
-              ),
-            ),
-          ),
-        ],
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class Barrier extends StatelessWidget {
+  final size;
+  const Barrier({Key? key, this.size}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 50,
+      height: size,
+      decoration: BoxDecoration(
+        color: Colors.green,
       ),
     );
   }
